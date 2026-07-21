dcc5cfb45f5ba219a6dee0b2be79acf034c46839
rainymon <snkim04@snu.ac.kr>
2026-07-21 17:42:12 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ st.set_page_config(
 
 ACTIVE_TIMELINE_SECONDS = 43.0
 LEAD_IN_SECONDS = 1.0
-INTRO_SECONDS = 2.0
+INTRO_SECONDS = 1.5
 
 SPECIAL_ARTICLES = {
     6:   {"file": "assets/article_006.jpg", "duration": 2.0, "scroll": False},

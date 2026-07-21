9e776c2ac9391184ed1048cadf323fd919d08372
rainymon <snkim04@snu.ac.kr>
2026-07-21 14:26:31 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -151,7 +151,7 @@ html = r"""
 
 .live-time {
   font-family: "Radley", Georgia, serif;
-  font-size: clamp(24px, 3vw, 42px);
+  font-size: clamp(27px, 3.4vw, 48px);
   font-style: italic;
   font-weight: 700;
   letter-spacing: 0;

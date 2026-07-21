936082e33d6943649d02a916e43dafd3f0d63768
rainymon <snkim04@snu.ac.kr>
2026-07-21 18:23:22 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -99,7 +99,7 @@ html = r"""
       <div class="timeline-labels">
         <span class="timeline-label label-start" style="left:0%">00:00</span>
         <span class="timeline-label" style="left:59.236111%">14:13</span>
-        <span class="timeline-label label-end" style="left:96.458333%">23:09</span>
+        <span class="timeline-label" style="left:96.458333%">23:09</span>
         
       </div>
     </div>
